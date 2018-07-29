3bb4bbe89f60e8995645471a41273f262910acd5
Poga Po <poga.bahamut@gmail.com>
2018-07-29 16:12:41 +0800

    add notify() to lua context

--- src/actor.rs
+++ src/actor.rs
@@ -27,37 +27,52 @@ impl LuaActor {
         let actor = LuaActor::new(&body)?;
         Result::Ok(actor)
     }
+
+    fn invoke_in_scope(
+        &mut self,
+        ctx: &mut Context<Self>,
+        func_name: &str,
+        arg: LuaMessage,
+    ) -> <Self as Handler<LuaMessage>>::Result {
+        self.vm.scope(|scope| {
+            let globals = self.vm.globals();
+
+            let notify = scope
+                .create_function_mut(|_, msg| {
+                    println!("notified! {:?}", msg);
+                    ctx.notify(msg);
+                    Ok(())
+                })
+                .unwrap();
+            globals.set("notify", notify).unwrap();
+
+            let lua_handle: Result<Function, LuaError> = globals.get(func_name);
+            if let Ok(f) = lua_handle {
+                LuaMessage::from_lua(f.call::<LuaMessage, Value>(arg).unwrap(), &self.vm).unwrap()
+            } else {
+                LuaMessage::Nil
+            }
+        })
+    }
 }
 
 impl Actor for LuaActor {
     type Context = Context<Self>;
 
-    fn started(&mut self, _: &mut Context<Self>) {
-        let globals = self.vm.globals();
-        let lua_handle: Result<Function, LuaError> = globals.get("started");
-
-        if let Ok(f) = lua_handle {
-            f.call::<(), ()>(()).unwrap();
-        }
+    fn started(&mut self, ctx: &mut Context<Self>) {
+        self.invoke_in_scope(ctx, "started", LuaMessage::Nil);
     }
 
-    fn stopped(&mut self, _: &mut Context<Self>) {
-        let globals = self.vm.globals();
-        let lua_handle: Result<Function, LuaError> = globals.get("stopped");
-
-        if let Ok(f) = lua_handle {
-            f.call::<(), ()>(()).unwrap();
-        }
+    fn stopped(&mut self, ctx: &mut Context<Self>) {
+        self.invoke_in_scope(ctx, "stopped", LuaMessage::Nil);
     }
 }
 
 impl Handler<LuaMessage> for LuaActor {
     type Result = LuaMessage;
 
-    fn handle(&mut self, msg: LuaMessage, _: &mut Context<Self>) -> Self::Result {
-        let globals = self.vm.globals();
-        let lua_handle: Function = globals.get("handle").unwrap();
-        LuaMessage::from_lua(lua_handle.call::<LuaMessage, Value>(msg).unwrap(), &self.vm).unwrap()
+    fn handle(&mut self, msg: LuaMessage, ctx: &mut Context<Self>) -> Self::Result {
+        self.invoke_in_scope(ctx, "handle", msg)
     }
 }
 
@@ -156,6 +171,38 @@ mod tests {
             let l2 = lua_addr.send(LuaMessage::Nil);
             Arbiter::spawn(l2.map(|res| {
                 assert_eq!(res, LuaMessage::from(3));
+                System::current().stop();
+            }).map_err(|e| println!("actor dead {}", e)));
+        }).map_err(|e| println!("actor dead {}", e)));
+
+        system.run();
+    }
+
+    #[test]
+    fn lua_actor_notify() {
+        let system = System::new("test");
+
+        let addr = LuaActor::new(
+            r#"
+        c = 0
+        function started ()
+            notify(100)
+        end
+
+        function handle(msg)
+            c = c + msg
+            return c
+        end
+        "#,
+        ).unwrap()
+            .start();
+
+        let l = addr.send(LuaMessage::from(1));
+        Arbiter::spawn(l.map(move |res| {
+            assert_eq!(res, LuaMessage::from(1));
+            let l2 = addr.send(LuaMessage::from(1));
+            Arbiter::spawn(l2.map(|res| {
+                assert_eq!(res, LuaMessage::from(102));
                 System::current().stop();
             }).map_err(|e| println!("actor dead {}", e)));
         }).map_err(|e| println!("actor dead {}", e)));
